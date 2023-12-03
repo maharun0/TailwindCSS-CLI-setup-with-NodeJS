56364acd11ce299fc238d1b41440bc8d869c43a1
maharun0 <maharun.afroz@northsouth.edu>
2023-12-03 18:59:47 +0600

    CSS has been added to the project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html' // Specify path of the HTML file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
